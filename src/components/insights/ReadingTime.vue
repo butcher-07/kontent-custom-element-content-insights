7d9d92e99b2b2b5f154f5672ce0ae9f594132a8b
Zbyšek Němec <butcher-07@users.noreply.github.com>
2020-11-22 23:37:20 +0100

    Update ReadingTime.vue

--- src/components/insights/ReadingTime.vue
+++ src/components/insights/ReadingTime.vue
@@ -27,7 +27,11 @@ export default {
   },
   methods: {
     analyzeContent(content) {
+      var startTime = new Date();
       let rt = this.countReadingTime(content);
+      var endTime = new Date();
+
+            console.log("Reading time: " + ((endTime - startTime) / 1000) + " seconds");
       this.readingTime = {
         time: rt.text,
         words: rt.words
